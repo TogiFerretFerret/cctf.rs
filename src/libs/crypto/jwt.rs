01bf7338ab5791d8a284c5241dd6870f3879c24d
Riverxia <riverdalecoder@outlook.com>
2026-06-20 17:19:45 -0400

    (acm) Currently Listening To:  -

--- src/libs/crypto/jwt.rs
+++ src/libs/crypto/jwt.rs
@@ -1,14 +1,28 @@
-use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _, DecodeError};
-use serde_json::{Error};
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _};
+use thiserror::Error;
 
-fn jwt64_encode(payload: &str) -> 
+fn jwt64_encode(payload: &[u8]) -> String {
+    URL_SAFE_NO_PAD.encode(payload)
+}
+
+fn jwt64_decode(payload: &[u8]) -> Result<Vec<u8>, JwtError> {
+    URL_SAFE_NO_PAD.decode(payload).map_err(JwtError::Base64DecodeError)
+}
+
+
 
 #[derive(Debug, Error)]
 pub enum JwtError {
+    #[error("Your token was assembled by a Slitheen")]
     InvalidFormat,
+    #[error("Your token failed to use its Psychic Paper")]
     InvalidSignature,
+    #[error("The token is dead")]
     Expired,
+    #[error("Your token has temporarily played the role of TARDIS")]
     NotYetValid,
-    InvalidJson(serde_json::Error),
-    Base64DecodeError(base64::DecodeError)
+    #[error("Your token is a human in a Dalek suit: {0}")]
+    InvalidJson(#[from] serde_json::Error),
+    #[error("Your token refused to be exterminated: {0}")]
+    Base64DecodeError(#[from] base64::DecodeError)
 }
