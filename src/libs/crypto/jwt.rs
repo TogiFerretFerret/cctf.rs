20eaeb0d2dbabbab265e2e7423c38fc6883d3a0a
Riverxia <riverdalecoder@outlook.com>
2026-06-20 12:40:19 -0400

    (acm) Currently Listening To: BTMC Reacts to Worst HR Player Rubik's Cube FC!!! - bmc

--- src/libs/crypto/jwt.rs
+++ src/libs/crypto/jwt.rs
@@ -1,2 +1,14 @@
-use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _};
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _, DecodeError};
+use serde_json::{Error};
 
+fn jwt64_encode(payload: &str) -> 
+
+#[derive(Debug, Error)]
+pub enum JwtError {
+    InvalidFormat,
+    InvalidSignature,
+    Expired,
+    NotYetValid,
+    InvalidJson(serde_json::Error),
+    Base64DecodeError(base64::DecodeError)
+}
