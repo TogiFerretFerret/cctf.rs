e7bc6b7b0a288d83057515ccefa4aa4cb3d1857c
Riverxia <riverdalecoder@outlook.com>
2026-06-21 19:41:32 -0400

    (acm) Currently Listening To:  -

--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -1,3 +1,4 @@
 pub mod crypto;
 pub mod types;
 pub mod repos;
+pub mod services;
---
+++ src/libs/services.rs
@@ -0,0 +1,4 @@
+use crate::libs::repos::{AccountRepo, TeamRepo, ChallengeRepo, SolveRepo, RepoError};
+use crate::libs::types::accounts::{Account, AccountId, AccountName, AccountEmail, AccountRole, CtfTimeUserProfile};
+
+
