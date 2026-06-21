b961d07e911a98986b58286c681f9672d7d2951d
Riverxia <riverdalecoder@outlook.com>
2026-06-21 18:47:50 -0400

    (acm) Currently Listening To:  -

---
+++ src/libs/types/scoreboard.rs
@@ -0,0 +1,34 @@
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use crate::libs::types::teams::TeamId;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ScoreboardEntry {
+    pub team_id: TeamId,
+    pub team_name: String,
+    pub points: f64,
+    pub last_solve_time: Option<i64>,
+    pub solves: Vec<String>, // list of challenge ids
+}
+
+// ctftime scoreboard export
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CtfTimeTaskStats {
+    pub points: u32,
+    pub time: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CtfTimeStandingsEntry {
+    pub pos: Option<u32>,
+    pub team: String,
+    pub score: f64,
+    #[serde(rename = "taskStats")]
+    pub task_stats: HashMap<String, CtfTimeTaskStats>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CtfTimeScoreboardExport {
+    pub tasks: Vec<String>,
+    pub standings: Vec<CtfTimeStandingsEntry>,
+}
