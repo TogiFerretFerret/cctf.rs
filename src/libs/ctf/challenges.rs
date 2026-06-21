a1ce327287f39c25af6c22fa2b9d534ed29c8e23
Riverxia <riverdalecoder@outlook.com>
2026-06-21 12:53:05 -0400

    (acm) Currently Listening To: Stereo Hearts (feat. Adam Levine) - Gym Class Heroes, Adam Levine

--- src/libs/ctf/challenges.rs
+++ src/libs/ctf/challenges.rs
@@ -1,9 +1,57 @@
 use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengeTitle(pub String);
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengeDescription(pub HtmlString);
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengeCategory(pub String);
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengeTag(pub String);
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum ScoringMode {
+    PointValue,
+    PointAttribution,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengePoints {
+    pub mode: ScoringMode,
+    pub equation: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengeAuthor {
+    pub id: String,
+    pub username: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengeHint {
+    pub content: HtmlString,
+    pub cost: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChallengeFile {
+    pub name: String,
+    pub url: String,
+    pub checksum_sha256: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum ChallengeRequirement {
+    Solve(String),
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Challenge {
     pub id: String,
-    pub title: String,
+    pub title: ChallengeTitle,
     pub description: ChallengeDescription,
     pub category: ChallengeCategory,
     pub points: ChallengePoints,
