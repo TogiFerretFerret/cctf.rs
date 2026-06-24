af223b685ebf3db3aa815e02357503d49e211be4
Riverxia <riverdalecoder@outlook.com>
2026-06-24 12:54:51 -0400

    (acm) Currently Listening To: Kawaikutegomen - Will Stetson

--- src/libs/api.rs
+++ src/libs/api.rs
@@ -461,6 +461,19 @@ mod tests {
             Ok(self.teams.read().await.values().cloned().collect())
         }
     }
+    
+    #[async_trait]
+    impl InstanceRepo for TestStore {
+        async fn find_active_flag(
+            &self,
+            _challenge_id: &str, 
+            _team_id: Option<&TeamId>,
+            _account_id: &AccountId,
+        ) -> Result<Option<String>, RepoError> {
+            Ok(None)
+        }
+    }
+
     #[async_trait]
     impl ChallengeRepo for TestStore {
         async fn find_by_id(&self, id: &str) -> Result<Option<Challenge>, RepoError> {
