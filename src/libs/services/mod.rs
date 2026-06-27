26d757cfcb4ee205e54fae524620b070a0e44816
Riverxia <riverdalecoder@outlook.com>
2026-06-26 22:26:36 -0400

    (acm) Currently Listening To:  -

--- src/libs/services/mod.rs
+++ src/libs/services/mod.rs
@@ -78,7 +78,9 @@ pub mod auth;
 pub mod instancer;
 pub mod scoreboard;
 pub mod solve;
+pub mod email;
 
 pub use auth::{AuthService, OAuthService};
 pub use scoreboard::ScoreboardService;
 pub use solve::SolveService;
+pub use email::{Email, EmailService, SmtpSenderClient, SmtpCatcherServer};
