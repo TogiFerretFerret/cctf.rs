44f367592ddf967d62e3997d7bfea92365995424
Riverxia <riverdalecoder@outlook.com>
2026-07-01 22:24:14 -0400

    (acm) Currently Listening To:  -

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use cctf_rs::libs::{
     api::{self, AppState, RateLimiter},
     repos::pg::PgStore,
     services::{
-        email::{HttpCatcher, HttpCatcherConfig},
         AuthService, ConfigService, OAuthService, ScoreboardService, SolveService,
-    }
-}
+        email::{HttpCatcher, HttpCatcherConfig},
+    },
+};
